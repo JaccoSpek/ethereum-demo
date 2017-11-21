b979cb8ccfed294e52ac180050878bbfcfa1472c
Jacco <jacco.spek@tno.nl>
2017-11-21 12:06:12 +0100

    Added update/upgrade

--- install_docker.sh
+++ install_docker.sh
@@ -6,4 +6,4 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 apt-get install docker-ce
 curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compos
+chmod +x /usr/local/bin/docker-compose

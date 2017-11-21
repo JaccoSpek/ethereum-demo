5fd48d15d577980404e3ca074cff017420d66fb6
Jacco <jacco.spek@tno.nl>
2017-11-21 12:00:07 +0100

    Added update/upgrade

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt-get update && apt-get upgrade
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
